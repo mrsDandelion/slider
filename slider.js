54b143153e57549eb43b08d65220f163b5610408
Darya_Drachuk <Darya_Drachuk@epam.com>
2018-11-27 10:50:26 +0300

    add const to script

--- slider.js
+++ slider.js
@@ -1,3 +1,10 @@
 (function(){
-    console.log($('button'));
+    const sliderUl = $('.slider').css('overflow','hidden').children('ul');
+    const imgs = sliderUl.find('img');
+    const imgWidth = imgs.first().width();
+    const imgLength = imgs.length;
+    let current = 1;
+    const totalImgsWidth = imgWidth * imgLength;
+
+    $('.wrapper-nav-slider').show();
 })()
