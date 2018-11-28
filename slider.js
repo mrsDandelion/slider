264e28c31ca2782825f542a1cc73692e5c809c49
Darya_Drachuk <Darya_Drachuk@epam.com>
2018-11-28 15:49:07 +0300

    add function transition

--- slider.js
+++ slider.js
@@ -1,4 +1,4 @@
-(function(){
+(function($){
     const sliderUl = $('.slider').css('overflow','hidden').children('ul');
     const imgs = sliderUl.find('img');
     const imgWidth = imgs.first().width();
@@ -7,19 +7,29 @@
     const totalImgsWidth = imgWidth * imgLength;
 
     $('.wrapper-nav-slider').show().find('button').on('click',function(){
-        const direction = $(this).data('dir');
+        let direction = $(this).data('dir');
+        let loc = imgWidth;
+
         (direction === 'next')? ++current: --current;
 
         if(current === 0) {
             current = imgLength;
             direction = 'next';
-        } else if (current - 1 = imgLength) {
+        } else if (current-1 === imgLength) {
             current = 1;
         }
-        transition(sliderUl);
+        transition(sliderUl, loc, direction);
     });
 
-    function transition(){
+    function transition(container, loc, direction){
+        let init;
 
+        if(direction && loc !== 0) {
+            init = (direction === 'next') ?  '-=' : '+='
+        }
+
+        container.animate({
+            'margin-left': init? (init + loc): init
+        })
     }
-})()
+})(jQuery)
