79705060e4ef176031e16cf2c9d23b44da67b5aa
Darya_Drachuk <Darya_Drachuk@epam.com>
2018-12-04 15:59:13 +0300

    create second version slider

--- slider.js
+++ slider.js
@@ -6,3 +6,16 @@ function Slider(container, nav){
     this.imgLength = this.imgs.length;
     this.current = 0;
 }
+
+Slider.prototype.transition = function(coords) {
+    this.container.animate({
+        'margin-left': coords || -(this.current * this.imgWidth)
+    })
+}
+
+Slider.prototype.setCurrent = function( dir ){
+    let pos = this.current;
+    pos += (~~(dir === 'next') || -1);
+    this.current = (pos < 0)? this.imgLength - 1 : pos % this.imgLength;
+    return pos;
+}
