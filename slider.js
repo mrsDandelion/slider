3981b68cdda6e98f922fa2437b241cc0d018a54f
Darya_Drachuk <Darya_Drachuk@epam.com>
2018-12-05 12:28:51 +0300

    create select and two animations (have to fix)

--- slider.js
+++ slider.js
@@ -6,17 +6,32 @@ function Slider(container, nav){
     this.imgLength = this.imgs.length;
     this.current = 0;
     this.previous = 0;
+    this.typeOfAnimation = 'opacity';
 }
 
-Slider.prototype.transition = function(coords) {
+Slider.prototype.animationOpacity = function(coords) {
+    this.container.find('li').css('opacity', 0);
+    this.container.find(`li:nth-of-type(${this.current})`).css('opacity', 1);
     this.container.css('margin-left', coords || -(this.current * this.imgWidth));
-    /*this.container.animate({
-        'margin-left': coords || -(this.current * this.imgWidth)
-    })*/
     this.container.find(`li:nth-of-type(${this.previous + 1})`).css('opacity', 0)
     this.container.find(`li:nth-of-type(${this.current + 1})`).animate({
         'opacity': 1
     }, 1000)
+}
+
+Slider.prototype.animationlinear = function(coords) {
+    this.container.animate({
+        'margin-left': coords || -(this.current * this.imgWidth)
+    })
+}
+
+Slider.prototype.transition = function() {
+    if(this.typeOfAnimation === 'opacity') {
+        this.animationOpacity();
+    }
+    if(this.typeOfAnimation === 'linear') {
+        this.animationlinear();
+    }
 }
 
 Slider.prototype.setCurrent = function( dir ){
@@ -25,4 +40,8 @@ Slider.prototype.setCurrent = function( dir ){
     pos += (~~(dir === 'next') || -1);
     this.current = (pos < 0)? this.imgLength - 1 : pos % this.imgLength;
     return pos;
+}
+
+Slider.prototype.setTypeOfAnimation = function(typeOfAnimation) {
+    this.typeOfAnimation = typeOfAnimation;
 }
